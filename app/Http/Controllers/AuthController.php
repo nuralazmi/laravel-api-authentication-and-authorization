171ed4a923954b782e7f441798e066367667e502
nuralazmi <nuralazmi@gmail.com>
2022-08-03 16:27:01 +0300

    Yeni üye kayıt ve üye girişi için rotalar oluşturuldu. Controller oluşturuldu ve register, login fonksiyonları yazıldı.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    //Yeni üye kayıt
+    public function register(Request $request): JsonResponse
+    {
+        /*
+         * Dışarıdan gelen parametreler kontrol edilecek. Validasyon işlemleri
+         * 4 parametre alınacak. Şifre 2 defa alınacak aynı olup olmadığı da kontrol edilecek.
+         */
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        //Eğer parametreler validasyon kurallarını karşılamazsa hata mesajları gösterilecek ve devam etmeyecek
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 400);
+        }
+
+        //Eğer validasyon başarılı ise kişi eklenecek.
+        User::create([
+            'name' => $request->input('name'),
+            'password' => bcrypt($request->input('password')),
+            'email' => $request->input('email')
+        ]);
+        return response()->json(['ok' => 'true']);
+    }
+
+    //Üye girişi
+    public function login(Request $request): JsonResponse
+    {
+        /*
+         * Dışarıdan gelen parametreler kontrol edilecek. Validasyon işlemleri
+         * * 4 parametre alınacak. Şifre 2 defa alınacak aynı olup olmadığı da kontrol edilecek.
+         */
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|',
+            'password' => 'required|string|min:6'
+        ]);
+
+        //Eğer parametreler validasyon kurallarını karşılamazsa hata mesajları gösterilecek ve devam etmeyecek
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors(),], 400);
+        }
+
+        //Gelen bilgilere ait üye var mı kontrol ediliyor.
+        if (!Auth::attempt($request->all())) {
+            return response()->json(['message' => 'Kayıt bulunamadı'], 401);
+        }
+
+        //Giriş başarılı. Token ile birlikte giriş yapılacak.
+        return response()->json(
+            [
+                'ok' => true,
+                'message' => 'Giriş başarılı',
+                'token' => auth()->user()->createToken('API Token')->plainTextToken
+            ]
+        );
+    }
+}
